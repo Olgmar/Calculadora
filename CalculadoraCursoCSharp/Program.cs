a9ac987004a0a40cc34d7497a1c04b8230a60c10
Your Name <you@example.com>
2020-04-06 11:59:26 -0300

    inicio

---
+++ CalculadoraCursoCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CalculadoraCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            string tmp;
+            int op;
+
+            Console.WriteLine("Digite um número: ");
+            tmp = Console.ReadLine();
+            num1 = int.Parse(tmp);
+
+            Console.WriteLine("Digite outro numero: ");
+            tmp = Console.ReadLine();
+            num2 = int.Parse(tmp);
+
+            double resultado;
+ 
+                resultado = num1 + num2;
+                Console.WriteLine("Soma: " + resultado);
+
+                resultado = num1 - num2;
+                Console.WriteLine("Subtracao: " + resultado);
+
+                resultado = num1 * num2;
+                Console.WriteLine("Multiplicacao: " + resultado);
+
+                resultado = num1 / num2;
+                Console.WriteLine("Divisao: " + resultado);
+        }
+    }
+}
